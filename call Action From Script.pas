56707ffe67c5d714a396d8290ebe4d366947bdcf
MySohoWiki <nz@mysoftwarehouse.gr>
2022-10-24 15:01:53 +0300

    CallAction

---
+++ call Action From Script.pas
@@ -0,0 +1,12 @@
+Procedure callAction;
+var                          
+  act: TActAction;
+begin
+  try
+    act:= TActAction.Create(nil);
+    act.load('{39929405-B13F-4113-930F-375FFF08E587}');
+    act.run(true,true);
+  except
+    act.free;
+    doerror(Exceptionmessage);
+  end; 
